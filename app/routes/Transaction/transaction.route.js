14d4aac2f9bf1c62ff0a364c337265ed0f58120c
Shyama patro <shyama.patro@gmail.com>
2024-08-08 20:54:55 +0530

    add

--- app/routes/Transaction/transaction.route.js
+++ app/routes/Transaction/transaction.route.js
@@ -3,7 +3,7 @@ const transaction = require("../../controllers/transactionController");
 const Validation= require("../Transaction/transactionValidation");
 
 module.exports = app => {
-  app.post(routeBaseUrl + "/createTransaction",Validation.createTransactionSchema,transaction.createTransaction);
+  app.post(routeBaseUrl + "/createTransaction", Validation.createTransactionSchema,transaction.createTransaction);
   app.get(routeBaseUrl + "/getListOfTransaction",  transaction.getListOfTransaction);
   app.get(routeBaseUrl + "/getTransactionDetail",  transaction.getTransactionDetail);
  }
